f075e50774792193e3289ad178231de00d1006d0
Ron DeMeritt <rdemeritt@gmail.com>
2026-04-15 14:03:30 -0400

    fix: remove redundant string enum values in SettingsKey

--- Sources/WhisKeyCore/Storage/SettingsKey.swift
+++ Sources/WhisKeyCore/Storage/SettingsKey.swift
@@ -2,11 +2,11 @@ import Foundation
 
 /// Known settings keys for the key-value settings store.
 public enum SettingsKey: String, Sendable, CaseIterable {
-    case whisperModel     = "whisperModel"
-    case languageHint     = "languageHint"
-    case llmProvider      = "llmProvider"
-    case toneStyle        = "toneStyle"
-    case removeFillers    = "removeFillers"
-    case addPunctuation   = "addPunctuation"
-    case rawMode          = "rawMode"
+    case whisperModel
+    case languageHint
+    case llmProvider
+    case toneStyle
+    case removeFillers
+    case addPunctuation
+    case rawMode
 }
